b0a7c0ed2a702ef1858bc34ddf8588d08e6872bd
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-05-08 13:28:43 +0100

    add observedAttributes to l-image-layer

--- src/l-image-overlay.js
+++ src/l-image-overlay.js
@@ -1,26 +1,40 @@
 import { mapAddTo } from "./events.js"
 
 class LImageOverlay extends HTMLElement {
+  static observedAttributes = ["url", "bounds", "opacity"]
+
   constructor() {
     super()
+    this.layer = null
   }
 
   connectedCallback() {
-    console.log("l-image-overlay connected")
     const url = this.getAttribute("url")
     const bounds = JSON.parse(this.getAttribute("bounds"))
     const options = {
       opacity: parseFloat(this.getAttribute("opacity") || "1.0"),
       alt: this.getAttribute("alt") || ""
     }
-    const layer = L.imageOverlay(url, bounds, options)
+    this.layer = L.imageOverlay(url, bounds, options)
     this.dispatchEvent(new CustomEvent(mapAddTo, {
       cancelable: true,
       bubbles: true,
       detail: {
-        layer
+        layer: this.layer
       }
     }))
+  }
+
+  attributeChangedCallback(name, _oldValue, newValue) {
+    if (this.layer !== null) {
+      if (name === "url") {
+        this.layer.setUrl(newValue)
+      } else if (name === "bounds") {
+        this.layer.setBounds(JSON.parse(newValue))
+      } else if (name === "opacity") {
+        this.layer.setOpacity(parseFloat(newValue))
+      }
+    }
   }
 }
 

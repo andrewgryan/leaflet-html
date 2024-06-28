d5a49f3759b6d2c8febebc0375e59ed2d8106be7
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-06-28 15:48:07 +0100

    use browser mode

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   test: {
+    browser: {
+      enabled: true,
+      headless: true,
+      name: "chromium",
+      provider: "playwright"
+    },
     includeSource: ["src/**/*.{js,ts}"],
   },
 });

dbf641617f61a18e9ba484b710709ca3bd1edc12
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-10-29 13:22:06 +0000

    switch to vite as build tool

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.js"),
+      name: "leaflet-html",
+      fileName: "leaflet-html"
+    }
+  },
   test: {
     includeSource: ["src/**/*.{js,ts}"],
     coverage: {
